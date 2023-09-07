40f6e65c114c9fdc8d2f87fb070723574f4f466e
Asher Azriel Ginting <asherginting3@gmail.com>
2023-09-08 00:00:57 +0700

    feat: add all

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { isOpenSidebar } from '$lib/stores/dashboardStore';
+	import Icon from '@iconify/svelte';
+
+	let isOpenNotifications = false;
+	let isOpenDropdown = false;
+</script>
+
+<header class="z-10 p-4 md:px-6 bg-white shadow-sm border-b">
+	<nav class="flex justify-between items-center">
+		<button
+			type="button"
+			on:click|stopPropagation={() => isOpenSidebar.set(true)}
+			class="text-2xl block md:hidden text-primary-700 rounded-lg"
+		>
+			<Icon icon="mingcute:menu-fill" class="text-3xl" />
+		</button>
+	</nav>
+</header>
---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import { isOpenSidebar } from '$lib/stores/dashboardStore';
+	import { page, navigating } from '$app/stores';
+	import { links } from '../data/links';
+	import NavDropdownLink from './NavDropdownLink.svelte';
+	import { clickoutside } from '@svelte-put/clickoutside';
+	import Icon from '@iconify/svelte';
+	import { PUBLIC_SITE_NAME } from '$env/static/public';
+
+	let routeId: string;
+
+	$: {
+		routeId = $page.url.pathname;
+		if ($navigating) isOpenSidebar.set(false);
+	}
+</script>
+
+<aside
+	use:clickoutside
+	on:clickoutside={() => isOpenSidebar.set(false)}
+	class="overflow-y-auto h-full fixed {$isOpenSidebar
+		? 'translate-x-0'
+		: '-translate-x-full'} duration-300 md:translate-x-0 md:static z-20 md:block w-60 md:w-56 lg:w-64 bg-white flex-shrink-0 border-r"
+>
+	<div class="py-6 relative">
+		<button
+			on:click={() => isOpenSidebar.set(false)}
+			class=" block md:hidden text-white p-1 text-xl absolute right-0 rounded-tl-md rounded-bl-md"
+		>
+			<Icon icon="uil:times" class="text-2xl" />
+		</button>
+		<a href="/" class="px-4 text-2xl font-bold text-primary-700">{PUBLIC_SITE_NAME}</a>
+		<ul class="mt-5">
+			{#each links as link}
+				<li>
+					{#if link.path}
+						<a
+							href={link.path}
+							class="flex items-center px-4 py-4 border-l-4 border-transparent {link.path == routeId
+								? '  text-primary-700'
+								: 'text-gray-500'} hover:bg-gray-50 hover:text-black font-medium block duration-200"
+						>
+							<Icon
+								icon={link.path == routeId ? link.icon.active : link.icon.inactive}
+								class="text-3xl mr-3"
+							/>
+							<span>{link.name}</span></a
+						>
+					{:else}
+						<NavDropdownLink nestedLinks={link?.nested_links} {link} {routeId} />
+					{/if}
+				</li>
+			{/each}
+		</ul>
+	</div>
+	<footer class="px-6 text-xs absolute bottom-6 text-gray-700">&copy; 2023 By Asher Azriel Ginting</footer>
+</aside>
+
+<style>
+    .text-primary-700 {
+        color: #605cff !important;
+    }
+</style>
